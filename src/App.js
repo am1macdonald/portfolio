3fbf0572cf0d157830de9f22c29e4628cb0dacc5
Adam MacDonald <am1macdonald@gmail.com>
2022-09-18 18:27:57 -0400

    remove top button

--- src/App.js
+++ src/App.js
@@ -1,18 +1,16 @@
 import React from "react";
 import About from "./components/About/About";
-import Contact from "./components/Contact";
-import Footer from "./components/Footer";
+import Contact from "./components/Contact/Contact";
+import Footer from "./components/Footer/Footer";
 import Hero from "./components/Hero/Hero";
 import Navbar from "./components/Navbar/Navbar";
 import Projects from "./components/Projects/Projects";
-import TopButton from "./components/TopButton/TopButton";
 import projectLibrary from "./projectLibrary";
 
 function App() {
   return (
     <div data-testid="app">
       <Navbar />
-      <TopButton />
       <Hero />
       <About />
       <Projects projectLibrary={projectLibrary} />
