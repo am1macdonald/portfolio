2853a67620ed913631878392fbdda7f222da5280
Adam MacDonald <am1macdonald@gmail.com>
2022-09-20 10:54:16 -0400

    import data to build the tiles for projects

--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -1,26 +1,46 @@
 import React from "react";
 import Styles from "./Projects.module.scss";
 import uniqid from "uniqid";
+import { useState } from "react";
+import { useEffect } from "react";
 
-const { projectSection, projectGrid, projectTile } = Styles;
+const { projectSection, projectGrid, projectTile, projectThumbnail } = Styles;
 
-const ProjectTile = ({ data }) => {
-  const { img, title, summary } = data;
+const ProjectTile = ({ project, getImage }) => {
+  const { thumbnail, live, repo, title } = project;
+
+  const [imageURL, setImageURL] = useState("");
+
+  useEffect(() => {
+    const getURL = async () => {
+      const result = await getImage(thumbnail);
+      setImageURL(result);
+      console.log(result);
+    };
+    getURL();
+  }, []);
 
   return (
     <div className={projectTile}>
-      <img src={img} alt={`${title}, thumbnail`} />
+      <img src={imageURL} alt="thumbnail" className={projectThumbnail} />
       <div>
         <h3>{title}</h3>
-        <p>{summary}</p>
+        <p>
+          Live: <a href={live}>{live}</a>
+        </p>
+        <p>
+          Repo: <a href={repo}>{repo}</a>
+        </p>
+        <summary></summary>
       </div>
     </div>
   );
 };
 
-const Projects = ({ projectLibrary }) => {
-  const projectList = projectLibrary.map((proj) => (
-    <ProjectTile key={uniqid} data={proj} />
+const Projects = ({ projects, getImage }) => {
+  console.log(projects);
+  const projectList = projects.map((project) => (
+    <ProjectTile key={uniqid()} project={project} getImage={getImage} />
   ));
 
   return (
