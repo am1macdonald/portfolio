716900648b49702c3eb9d67e8436e7741d1ee937
Adam MacDonald <am1macdonald@gmail.com>
2022-09-20 10:52:56 -0400

    add firebase config and funtions to fetch data

--- src/App.js
+++ src/App.js
@@ -1,19 +1,48 @@
 import React from "react";
+import { useEffect } from "react";
+import { useState } from "react";
 import About from "./components/About/About";
 import Contact from "./components/Contact/Contact";
 import Footer from "./components/Footer/Footer";
 import Hero from "./components/Hero/Hero";
 import Navbar from "./components/Navbar/Navbar";
 import Projects from "./components/Projects/Projects";
-import projectLibrary from "./projectLibrary";
+import {
+  getImageFromStorage,
+  getProjectCollection,
+} from "./firebase/firebase_config";
 
 function App() {
+  const [summary, setSummary] = useState("");
+  const [statement, setStatement] = useState("");
+  const [projects, setProjects] = useState([]);
+
+  useEffect(() => {
+    fetch("Summary.md")
+      .then((result) => result.text())
+      .then((text) => setSummary(text));
+  }, []);
+
+  useEffect(() => {
+    fetch("Statements.md")
+      .then((result) => result.text())
+      .then((text) => setStatement(text));
+  });
+
+  useEffect(() => {
+    const fetchProjects = async () => {
+      let result = await getProjectCollection();
+      setProjects(result);
+    };
+    fetchProjects();
+  }, []);
+
   return (
     <div data-testid="app">
       <Navbar />
       <Hero />
-      <About />
-      <Projects projectLibrary={projectLibrary} />
+      <About summary={summary} statement={statement} />
+      <Projects projects={projects} getImage={getImageFromStorage} />
       <Contact />
       <Footer />
     </div>
