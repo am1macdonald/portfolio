29143af11b522b40a6326ba76729df78c16a7440
Adam MacDonald <am1macdonald@gmail.com>
2023-06-18 19:39:08 -0400

    Update packages

--- tests/test.ts
+++ tests/test.ts
@@ -1,6 +1,10 @@
 import { expect, test } from '@playwright/test';
 
-test('index page has expected h1', async ({ page }) => {
+test('index page has expected header', async ({ page }) => {
 	await page.goto('/');
-	await expect(page.getByRole('heading', { name: 'Welcome to SvelteKit' })).toBeVisible();
+	await expect(page.getByRole('application', { name: 'Welcome to SvelteKit' })).toBeVisible();
+});
+test('index page has expected footer', async ({ page }) => {
+	await page.goto('/');
+	await expect(page.getByRole('footer', { name: 'Welcome to SvelteKit' })).toBeVisible();
 });
