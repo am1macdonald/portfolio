f4ad4ac1f650f882a0a808522348f7edecf757e2
Adam MacDonald <am1macdonald@gmail.com>
2023-06-20 22:01:59 -0400

    Update projects

--- src/lib/Project/Projects.svelte
+++ src/lib/Project/Projects.svelte
@@ -7,6 +7,7 @@
 
 <section class="projectSection">
 	<h2>Projects</h2>
+	<p>Coming soon...</p>
 	<div id="projects" class="projectGrid">
 		{#each projects as project}
 			<ProjectTile props={project} />
