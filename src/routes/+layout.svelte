e79998e3b0cae889a8160d0bf9cc7dbff63b1f08
Adam MacDonald <am1macdonald@gmail.com>
2023-06-21 07:45:59 -0400

    Add Vercel analytics

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -7,6 +7,10 @@
 	import Navbar from '$lib/Navbar.svelte';
 	import Contact from '$lib/Contact.svelte';
 	import Projects from '$lib/Project/Projects.svelte';
+  import { dev } from '$app/environment';
+  import { inject } from '@vercel/analytics';
+
+  inject({ mode: dev ? 'development' : 'production' });
 </script>
 
 <Navbar slot="navbar" />
